d8a810bb91856bba717f5945d3224d56156d35c7
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-12-13 11:25:23 +0700

    creating on computed methods

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Game from
 
 Vue.use(VueRouter)
 
@@ -11,7 +12,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
+    path: '/game/:room',
+    name: 'game',
+    component: Game
+  },
+  {
+    path: '/about/:room',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
--- src/views/About.vue
+++ src/views/About.vue
@@ -51,3 +51,78 @@
   </div>
 
 </template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      formusername: true,
+      newuser: '',
+      roomThisLink: `/game/${this.$route.params.room}`
+    }
+  },
+  computed: {
+    // getData () {
+    //   const data = Object.values(this.$store.state.objectData)
+    //   const arr = data.slice(2, data.length - 1)
+    //   // console.log(arr, "ini dari data object value");
+    //   for (var i = 0; i < arr.length; i++) {
+    //     for (var j = i + 1; j < arr.length; j++) {
+    //       if (arr[j].playerOrder > arr[i].playerOrder) {
+    //         var tampung = arr[i]
+    //         arr[i] = arr[j]
+    //         arr[j] = tampung
+    //       }
+    //     }
+    //   }
+    //   return arr
+    // },
+    // getTotalPlayer () {
+    //   return this.$store.state.objectData.count
+    // },
+    getUser () {
+      return this.$store.state.username
+    },
+    startGame () {
+      return this.$store.state.playStatus
+    },
+    getMaster () {
+      return this.$store.state.master
+    }
+  },
+  watch: {
+    startGame (value) {
+      if (value) {
+        this.$router.push({ path: `/game/${this.$route.params.room}` })
+      }
+    }
+  },
+  methods: {
+    fetchData () {
+      this.$store.dispatch('fetchData', this.$route.params.room)
+    },
+    userJoinRoom () {
+      this.$store.dispatch('addPlayer', { room: this.$route.params.room, newuser: this.newuser })
+    },
+    playNow () {
+      this.$store.dispatch('playNow', this.$route.params.room)
+    }
+  },
+  created () {
+    this.fetchData()
+  }
+}
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+
+  .welcome-title{
+    font-family: 'Quicksand', sans-serif;
+  }
+  .background-color-XXX{
+    background-color: #62B69B;
+  }
+</style>
